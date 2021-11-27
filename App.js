dc6d8ede3f28e4202317dd22e191df96dfb8cc3f
Thalia Silva <thalia.yuuki@gmail.com>
2021-11-27 12:24:25 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import {Aluno as Curso} from './components/Cabeçalho'
-import Aluno from './components/Aluno'
-import Imagem from './components/Corpo'
-import Disciplinas from './components/Disciplina'
+import IMC from './components/12IMC/CalculadoraApp'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Curso nome='Design'/>
-      <Aluno />
-      <Imagem />
-      <Disciplinas />
+      <IMC/>
     </View>
   );
 }
